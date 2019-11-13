74cec83d8e4f23b988b0578173ff4578d6d023ec
Akshay R <46285388+appdevdude7@users.noreply.github.com>
2019-11-13 07:28:20 -0600

    Updated chart data

--- src/example.js
+++ src/example.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { PieChart } from 'reaviz';
+import React from "react";
+import { PieChart } from "reaviz";
 
 export const data = [
   {
-    key: 'Phishing Attack',
+    key: "Apple",
     data: 10
   },
   {
-    key: 'IDS',
+    key: "NetFlix",
     data: 14
   },
   {
-    key: 'Malware',
+    key: "FaceBook",
     data: 5
   },
   {
-    key: 'DLP',
+    key: "Amazon",
     data: 18
   }
 ];
 
 export default () => (
-  <div style={{ margin: '55px', textAlign: 'center' }}>
-    <PieChart width={250} height={250} data={data} />
+  <div style={{ margin: "55px", textAlign: "center" }}>
+    <h1>Portfolio Holdings</h1>
+    <PieChart width={500} height={500} data={data} />
   </div>
 );
